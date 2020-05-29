3497d3839f054ebe00921f3fa56aaaea773e4e54
Michel Caron <mitchou_114@hotmail.com>
2020-05-29 08:57:10 -0400

    append ExecInfo to Mail body

--- Logging/targets/Email.ps1
+++ Logging/targets/Email.ps1
@@ -35,6 +35,35 @@
         if ($Log.Body) {
             $Params.Body += "`n`n{0}" -f ($Log.Body | ConvertTo-Json)
         }
+        if ($Log.ExecInfo) {
+            $ExceptionFormat = "{0}`n" +
+                               "{1}`n" +
+                               "+     CategoryInfo          : {2}`n" +
+                               "+     FullyQualifiedErrorId : {3}`n" +
+                               "+     Details message       : {6}`n" +
+                               "+     RecommendedAction     : {7}`n" +
+                               "`n`n" +
+                               "ScriptStackTrace :`n" +
+                               "{4}"+
+                               "`n`n" +
+                               "Exception details :" +
+                               "{5}"
+
+            $ExceptionFields = @($Log.ExecInfo.Exception.Message,
+                               $Log.ExecInfo.InvocationInfo.PositionMessage,
+                               $Log.ExecInfo.CategoryInfo.ToString(),
+                               $Log.ExecInfo.FullyQualifiedErrorId,
+                               $Log.ExecInfo.ScriptStackTrace,
+                               ($Log.ExecInfo.Exception | format-list -force | Out-String),
+                               $Log.ExecInfo.ErrorDetails.message,
+                               $Log.ExecInfo.ErrorDetails.RecommendedAction)
+
+            if ( [string]::IsNullOrEmpty($Params['Body']) ){
+                $Params['Body'] = $ExceptionFormat -f $ExceptionFields
+            } else {
+                $Params['Body'] += "`n`n" + ($ExceptionFormat -f $ExceptionFields)
+            }
+        }
 
         Send-MailMessage @Params
     }
