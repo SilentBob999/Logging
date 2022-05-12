e545052a8e06ef782b7e9fdc0b866aea2fca79a4
Michel Caron <mitchou_114@hotmail.com>
2022-05-12 15:41:18 -0400

    log email - correction after 4.8.5 squash
    commit look ugly _ after squash correction that do :
    - keep the html stuff from the squash
    - bring back the formating I had added and got remove (using Format-Pattern  this time)
    - bring back my full error log information and formating (now place in a different spot of the code as the squash added new ExecInfo from upstream...)

--- Logging/targets/Email.ps1
+++ Logging/targets/Email.ps1
@@ -19,13 +19,32 @@
             [hashtable] $Configuration
         )
 
-        $Body = '<h3>{0}</h3>' -f $Log.Message
+        $Body = '<h3>{0}</h3>' -f (Format-Pattern -Pattern $Configuration.Format -Source $Log)
 
         if (![String]::IsNullOrWhiteSpace($Log.ExecInfo)) {
-            $Body += '<pre>'
-            $Body += "`n{0}" -f $Log.ExecInfo.Exception.Message
-            $Body += "`n{0}" -f (($Log.ExecInfo.ScriptStackTrace -split "`r`n" | % { "`t{0}" -f $_ }) -join "`n")
-            $Body += '</pre>'
+            $ExceptionFormat = "{0}`n" +
+                               "{1}`n" +
+                               "+     CategoryInfo          : {2}`n" +
+                               "+     FullyQualifiedErrorId : {3}`n" +
+                               "+     Details message       : {6}`n" +
+                               "+     RecommendedAction     : {7}`n" +
+                               "`n`n" +
+                               "ScriptStackTrace :`n" +
+                               "{4}"+
+                               "`n`n" +
+                               "Exception details :" +
+                               "{5}"
+
+            $ExceptionFields = @($Log.ExecInfo.Exception.Message,
+                               $Log.ExecInfo.InvocationInfo.PositionMessage,
+                               $Log.ExecInfo.CategoryInfo.ToString(),
+                               $Log.ExecInfo.FullyQualifiedErrorId,
+                               (($Log.ExecInfo.ScriptStackTrace -split "`r`n" | % { "`t{0}" -f $_ }) -join "`n"),
+                               ($Log.ExecInfo.Exception | format-list -force | Out-String),
+                               $Log.ExecInfo.ErrorDetails.message,
+                               $Log.ExecInfo.ErrorDetails.RecommendedAction)
+
+            $Body += "<pre>`n{0}</pre>" -f ((($ExceptionFormat -f $ExceptionFields) -split "`r`n" | % { "`t{0}" -f $_ }) -join "`n")
         }
 
         $Params = @{
@@ -45,35 +64,6 @@
 
         if ($Log.Body) {
             $Params.Body += "`n`n{0}" -f ($Log.Body | ConvertTo-Json)
-        }
-        if ($Log.ExecInfo) {
-            $ExceptionFormat = "{0}`n" +
-                               "{1}`n" +
-                               "+     CategoryInfo          : {2}`n" +
-                               "+     FullyQualifiedErrorId : {3}`n" +
-                               "+     Details message       : {6}`n" +
-                               "+     RecommendedAction     : {7}`n" +
-                               "`n`n" +
-                               "ScriptStackTrace :`n" +
-                               "{4}"+
-                               "`n`n" +
-                               "Exception details :" +
-                               "{5}"
-
-            $ExceptionFields = @($Log.ExecInfo.Exception.Message,
-                               $Log.ExecInfo.InvocationInfo.PositionMessage,
-                               $Log.ExecInfo.CategoryInfo.ToString(),
-                               $Log.ExecInfo.FullyQualifiedErrorId,
-                               $Log.ExecInfo.ScriptStackTrace,
-                               ($Log.ExecInfo.Exception | format-list -force | Out-String),
-                               $Log.ExecInfo.ErrorDetails.message,
-                               $Log.ExecInfo.ErrorDetails.RecommendedAction)
-
-            if ( [string]::IsNullOrEmpty($Params['Body']) ){
-                $Params['Body'] = $ExceptionFormat -f $ExceptionFields
-            } else {
-                $Params['Body'] += "`n`n" + ($ExceptionFormat -f $ExceptionFields)
-            }
         }
 
         Send-MailMessage @Params
