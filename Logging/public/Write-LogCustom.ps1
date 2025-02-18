ca9a220c666b4da0e8bb53c0308800ef344e24d4
Michel Caron (CUSM) <Michel.Caron-IS@muhc.mcgill.ca>
2025-02-18 13:41:36 -0500

    add Write-LogCustom and  Write-LogOutputStream
    used to have those function outside the module Logging

---
+++ Logging/public/Write-LogCustom.ps1
@@ -0,0 +1,132 @@
+﻿<#
+.SYNOPSIS
+Write-LogCustom is a sort of overload of Write-Log (module Logging)
+
+.DESCRIPTION
+This function differ from Write-Log as it determine a Unique Event ID for each event (dynamictly).
+All the event ID are save to LoggingConfigEventID.xml
+
+.PARAMETER Message
+Test to display
+
+.PARAMETER Arguments
+? Directly pass to Write-Log (module Logging). Refer to the module doc.
+
+.PARAMETER Level
+'ERROR','WARNING','INFO','DEBUG'
+Set the color in the console and the level in the Event Log
+
+.PARAMETER ExceptionInfo
+Of type ErrorRecord. Pass the object to get more detail info in the log.
+
+.EXAMPLE
+Write-LogCustom -Level WARNING -Message "attention, we missing this..."
+try {throw "error"} catch {Write-LogCustom -Level ERROR -Message "error encounter" -ExceptionInfo $_}
+
+.NOTES
+General notes
+#>
+
+Function Write-LogCustom {
+    param (
+        [Parameter(Position = 1,
+        Mandatory = $false)]
+        [alias('msg')]
+        [string]$Message,
+        [Parameter(Position = 2,
+        Mandatory = $false)]
+        [alias('arg')]
+        [array] $Arguments,
+        [Parameter(Position = 4,
+            Mandatory = $false)]
+        [ValidateSet('ERROR','WARNING','INFO','DEBUG')]
+        [alias('lev')]
+        [string]$Level,
+        [Parameter(Position = 5,
+            Mandatory = $false)]
+        [alias('err')]
+        [System.Management.Automation.ErrorRecord]$ExceptionInfo,
+        [Parameter(Position = 6,
+            Mandatory = $false)]
+        [alias('bscope')]
+        [int]$BumpCallerScope=0,
+        [Parameter(Position = 7,
+            Mandatory = $false)]
+        [System.ConsoleColor]$ForegroundColor,
+        [Parameter(Position = 8,
+            Mandatory = $false)]
+        [System.ConsoleColor]$BackgroundColor
+    )
+    $BumpCallerScope = $BumpCallerScope + 1
+
+    $Params = @{
+       Body = @{ EventId = 0 }
+    }
+    if ($ExceptionInfo) {
+        $Params['ExceptionInfo'] = $ExceptionInfo
+    }
+    if ($Message) {
+        $Params['Message'] = $Message
+    }
+    if ($Arguments) {
+        $Params['Arguments'] = $Arguments
+    }
+    if ($Level) {
+        $Params['Level'] = $Level
+    }
+    if ($BumpCallerScope) {
+        $Params['BumpCallerScope'] = $BumpCallerScope
+    }
+    if ($ForegroundColor) {
+        $Params['ForegroundColor'] = $ForegroundColor
+    }
+    if ($BackgroundColor) {
+        $Params['BackgroundColor'] = $BackgroundColor
+    }
+
+    ### REGION set EventIdentifierName ###
+    $invocationInfo = (Get-PSCallStack)[$($BumpCallerScope + 1)]
+    try {$File = (Get-Item $($invocationInfo.ScriptName)).Name}
+    catch {$File = ""}
+    $Info = "[$($File) -> $($invocationInfo.Command)]"
+    if ($ExceptionInfo) {
+        $EventIdentifierName = "$Info" + "$($ExceptionInfo.Exception.Message)"
+      #  $EventIdentifierName = "$Info" + "$($ExceptionInfo.Exception.GetType().fullname)"
+    } elseif ($null -ne $Level -and $Level -notlike "INFO") {
+        $EventIdentifierName = "$Info" + "$Level"
+    } else { $EventIdentifierName = "$Info" + "OK" }
+    ### END REGION ###
+
+    ### REGION INITIALIZE ID LIST ###
+    if (-not (Test-Path Variable:Global:EventIdList)) {
+        if (-not (Test-Path Variable:Global:EventIDPath)) {
+            $Global:EventIDPath = Join-Path (Split-Path -Parent (Get-PSCallStack)[$($BumpCallerScope  )].ScriptName) LoggingConfigEventID.xml
+        }
+        if (Test-Path $Global:EventIDPath) {
+            $Global:EventIdList = Import-Clixml -Path $Global:EventIDPath
+        } else {
+            $Global:EventIdList = @{}
+        }
+    }
+    Write-Verbose $Global:EventIDPath
+    ### END REGION ###
+
+    ### REGION GET ID ###
+    if ($Global:EventIdList["$EventIdentifierName"]){
+        $Params['Body'] = @{ EventId = $Global:EventIdList["$EventIdentifierName"] }
+        #Write-Verbose "Found event ""$($EventIdentifierName)"" with ID $($Global:EventIdList["$EventIdentifierName"])"
+    } else {
+        ### SUB-REGION Create a new ID ###
+        if ( (($Global:EventIdList).Count -eq 0) ){$id = 10} else {
+           $id = (@($Global:EventIdList.Values  | Sort-Object -Descending)[0]) + 1
+        }
+        $Global:EventIdList["$EventIdentifierName"] = $id
+        $Global:EventIdList | Export-Clixml -Path $Global:EventIDPath
+        ### END SUB REGION ###
+        $Params['Body'] = @{ EventId = $Global:EventIdList["$EventIdentifierName"] }
+        Write-Verbose "Did not found event ""$($EventIdentifierName)"", give new ID $($Global:EventIdList["$EventIdentifierName"])"
+    }
+    ### END REGION ###
+
+    return ( Write-Log @Params )
+}
---
+++ Logging/public/Write-LogOutputStream.ps1
@@ -0,0 +1,113 @@
+﻿<#
+.SYNOPSIS
+Capture stream and write-to log the corresponding record.
+
+.DESCRIPTION
+Catch VerboseRecord, DebugRecord, WarningRecord, ErrorRecord from the stream and log it.
+In vscode, *>&1 also catch write host, so InformationRecord are pass back to write-host (but we lossing color information)
+The rest is pass to the regular output stream.
+
+Use :
+Log all Warning to eventlog.
+Log all Verbose or Debug to eventlog.
+Log Error to event log without using try/catch when ErrorAction is set to continu.
+Etc...
+
+.PARAMETER input
+Anything pass from the pipeline.
+
+To redirect all the output of your script to the input of this function, use it this way :
+ "*>&1 | Write-LogOutputStream"
+Note that InformationRecord does not contain color information.
+
+.PARAMETER LogHost
+With this [SWITCH], every InformationRecord are write as information to log instead of just write back to host.
+
+.PARAMETER ForegroundColor
+The write-host Foreground Color to use when writting InformationRecord to host.
+Cannot be use in combinaison with LogHost switch.
+
+.EXAMPLE
+Write-Verbose "verbose message test" -Verbose *>&1 | Write-LogOutputStream
+
+.EXAMPLE
+$Scriptblock = [scriptblock]{
+        Write-Verbose "verbose message test" -Verbose
+        Write-Output "Regular output"
+        write-host "write to Host" -ForegroundColor Green
+}
+$Output = (. $Scriptblock *>&1 | Write-LogOutputStream -ForegroundColor DarkMagenta)
+write-host "Output is : $Output" -ForegroundColor Cyan
+
+.EXAMPLE
+$VerbosePreference = "Continue"
+switch ($VerbosePreference) {
+    "Continue" { $Verbose = $true }
+    Default { $Verbose = $false }
+}
+$Scriptblock = [scriptblock]{
+        [CmdletBinding()]
+        param()
+        Write-Verbose "verbose message test"
+        Write-Output "Regular output"
+        write-host "write to Host" -ForegroundColor Green
+}
+$Output = (. $Scriptblock -Verbose:$Verbose *>&1 | Write-LogOutputStream)
+write-host "Output is : $Output" -ForegroundColor Cyan
+
+.NOTES
+See this link for more information regarding Streams redirection :
+https://devblogs.microsoft.com/scripting/understanding-streams-redirection-and-write-host-in-powershell/
+#>
+
+Function Write-LogOutputStream {
+    [CmdletBinding(DefaultParameterSetName='NONE')]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        $input,
+   #     [Parameter(ParameterSetName='LogHost')]
+        [switch]$LogHost,
+      #  [Parameter(ParameterSetName='NotLogHost')]
+        [System.ConsoleColor]$ForegroundColor = $host.UI.RawUI.ForegroundColor,
+        [System.ConsoleColor]$BackgroundColor = $host.UI.RawUI.BackgroundColor
+    )
+    process {
+        $i = $input
+        foreach ($i in $input) {
+            if ( ($i -is [System.Management.Automation.VerboseRecord]) ) {
+                if ($null -eq $i.MessageData.ForegroundColor) {
+                    Write-LogCustom -Message $i -Level INFO -BumpCallerScope 1 -Verbose:$VerbosePreference -ForegroundColor Cyan
+                } else {
+                    Write-LogCustom -Message $i -Level INFO -BumpCallerScope 1 -Verbose:$VerbosePreference -ForegroundColor $i.MessageData.ForegroundColor
+                }
+
+            } elseif ( ($i -is [System.Management.Automation.DebugRecord]) ) {
+                Write-LogCustom -Message $i -Level DEBUG -BumpCallerScope 1 -Verbose:$VerbosePreference
+            } elseif ( ($i -is [System.Management.Automation.ErrorRecord]) ) {
+                Write-LogCustom -Message $i -ExceptionInfo $i -Level ERROR -BumpCallerScope 1 -Verbose:$VerbosePreference
+            } elseif ( ($i -is [System.Management.Automation.WarningRecord]) ) {
+                Write-LogCustom -Message $i -Level WARNING -BumpCallerScope 1
+            } elseif ( ($i -is [System.Management.Automation.InformationRecord]) -or ($i -is [System.String]) ) {
+                if ($LogHost) {
+                    if ($null -eq $i.MessageData.ForegroundColor) {
+                        Write-LogCustom -Message $i -Level INFO -BumpCallerScope 1 -ForegroundColor $ForegroundColor -BackgroundColor $BackgroundColor -Verbose:$VerbosePreference
+                    } else{
+                        Write-LogCustom -Message $i -Level INFO -BumpCallerScope 1 -ForegroundColor $i.MessageData.ForegroundColor -BackgroundColor $i.MessageData.BackgroundColor -Verbose:$VerbosePreference
+                    }
+
+                } else {
+                    Wait-Logging
+                    if ($null -eq $i.MessageData.ForegroundColor) {
+                        Write-Host -Object $i -ForegroundColor  $ForegroundColor -BackgroundColor $BackgroundColor
+                    } else {
+                        Write-Host -Object $i -ForegroundColor $i.MessageData.ForegroundColor -BackgroundColor $i.MessageData.BackgroundColor
+                    }
+                }
+            } else {
+                Wait-Logging
+                Write-Output $i
+            }
+       }
+       Wait-Logging
+    }
+}
