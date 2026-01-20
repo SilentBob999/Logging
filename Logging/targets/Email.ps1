f2493e1e61b0ca8924f371a66432dde296a2a469
SCHTSK-KYOCERA-PF <printersfaxes@muhc.mcgill.ca>
2026-01-20 16:14:27 -0500

    logging email subject compatibility
    new line not accepted in mail subject, fix subject to keep just what on first line

--- Logging/targets/Email.ps1
+++ Logging/targets/Email.ps1
@@ -53,7 +53,7 @@
             To         = $Configuration.To.Split(',').Trim()
             Port       = $Configuration.Port
             UseSsl     = $Configuration.UseSsl
-            Subject    = Format-Pattern -Pattern $Configuration.Subject -Source $Log
+            Subject    = ((Format-Pattern -Pattern $Configuration.Subject -Source $Log) -split "[\r|\n]")[0].Trim()
             Body       = $Body
             BodyAsHtml = $true
         }
